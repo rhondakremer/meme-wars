943b450dc864cd388fd2dd7a7ccc12fb62aa981c
andreia <andreia@Andreias-MacBook-Pro.local>
2019-11-13 20:43:21 -0500

    added buttons to memes on battlepage

--- client/src/components/NodeMailer/index.js
+++ client/src/components/NodeMailer/index.js
@@ -35,3 +35,40 @@ module.exports = function(app) {
     });
   });
 };
+
+var nodemailer = require("nodemailer");
+var http = require('http');
+var fs = require('fs');
+
+module.exports = function(app) {
+  app.post("/email/send", function(req, res) {
+    var transporter = nodemailer.createTransport({
+      service: req.body.serviceInput,
+      auth: {
+        user: req.body.email,
+        pass: req.body.password
+      }
+    });
+
+    var mailOptions = {
+      from: req.body.email,
+      to: "rhondakremer@gmail.com",
+      subject: req.body.subject,
+      text: req.body.message,
+      attachments: [
+        {
+          path: req.body.path
+        }
+      ]
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+        res.send("email sent");
+      }
+    });
+  });
+};
