30d8dd3653392a374675641e3bfe8be8a5c612bb
KREMER <331943@dadeschools.net>
2020-10-02 21:15:45 -0400

    format invite, login, mememaker containers

--- client/src/containers/MemeMaker/index.js
+++ client/src/containers/MemeMaker/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import UserProfile from '../../components/UserProfile';
 import "./style.css";
 import NavBar from '../../components/NavBar';
-import { Modal, ModalHeader, ModalBody, FormGroup, Label} from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
 import Api from '../../utils/API';
 
 const initialState = {
@@ -35,57 +35,47 @@ class MemeMaker extends Component {
 
     componentDidMount() {
         Api.getUsers()
-        .then(res => {
-            let user = JSON.parse(localStorage.getItem('session'));
-            this.setState({currentUser: user.id, createdBy: user.id, images:res.data,currentImage:res.data.length>0?res.data[0]:null})
-        }); 
-      }
+            .then(res => {
+                let user = JSON.parse(localStorage.getItem('session'));
+                this.setState({ currentUser: user.id, createdBy: user.id, images: res.data, currentImage: res.data.length > 0 ? res.data[0] : null })
+            });
+    }
 
-    //   componentDidMount() {
-       
-    //   }
-
-      getUserImg() {
+    getUserImg() {
         let images = [];
         for (let i = 0; i < this.state.users.length; i++) {
-          images.push(this.state.users[i].image)
-        } this.setState({images: images}, () => console.log(this.state))
-      }
-
-    saveMeme= () => {
-        Api
-            .saveMeme(this.state.baseImgURL, this.state.toptext, this.state.topY, this.state.topX, this.state.bottomtext, this.state.bottomY, this.state.bottomX, this.state.createdBy, this.state.imageOf)
-            .then(memeSaved => {
-        Api.
-            startBattle(memeSaved.data._id, memeSaved.data.createdBy, memeSaved.data.imageOf)
-            .then(
-                newBattleInitiated => console.log("we did a thing", newBattleInitiated))
-                // debugger;
-                // console.log("HOW DOES THIS LOOK?", JSON.stringify(memeSaved.data._id));
-                alert("Yay! Your meme has been added")
-                this.setState({modalIsOpen: false})
-        })
+            images.push(this.state.users[i].image)
+        }
+        this.setState({ images: images }, () => console.log(this.state))
     }
-    
-    _imageEncode (arrayBuffer) {
-        let b64encoded = btoa([].reduce.call(new Uint8Array(arrayBuffer),function(p,c){return p+String.fromCharCode(c)},''))
-        let mimetype="image/jpeg"
-        return "data:"+mimetype+";base64,"+b64encoded
+
+    saveMeme = () => {
+        Api.saveMeme(this.state.baseImgURL, this.state.toptext, this.state.topY, this.state.topX, this.state.bottomtext, this.state.bottomY, this.state.bottomX, this.state.createdBy, this.state.imageOf)
+            .then(memeSaved => {
+                Api.startBattle(memeSaved.data._id, memeSaved.data.createdBy, memeSaved.data.imageOf)
+                    .then(
+                        newBattleInitiated => console.log("we did a thing", newBattleInitiated)
+                    );
+                alert("Yay! Your meme has been added");
+                this.setState({ modalIsOpen: false });
+            })
+    }
+
+    _imageEncode(arrayBuffer) {
+        let b64encoded = btoa([].reduce.call(new Uint8Array(arrayBuffer), function (p, c) { return p + String.fromCharCode(c) }, ''))
+        let mimetype = "image/jpeg"
+        return "data:" + mimetype + ";base64," + b64encoded
     }
 
     openImage = (image) => {
-        
-        this.setState({baseImgURL:image.image}, () => {
-            // console.log( 'baseimgurl', this.state.baseImgURL)
-            Api.getUserFromImage(image.id).then(
-            user => {
-
-                // console.log("this is the response", user.data._id)
-                this.setState({imageOf:user.data._id}, () => console.log("imageof set", this.state.imageOf))
-            })
+        this.setState({ baseImgURL: image.image }, () => {
+            Api.getUserFromImage(image.id)
+                .then(user => {
+                    this.setState({ imageOf: user.data._id }, () => console.log("imageof set", this.state.imageOf))
+                })
         });
 
-        Api.downloadImage(image.image).then(imageData=>{
+        Api.downloadImage(image.image).then(imageData => {
             this.setState(prevState => ({
                 currentImage: image,
                 modalIsOpen: !prevState.modalIsOpen,
@@ -108,8 +98,7 @@ class MemeMaker extends Component {
     }
 
     getStateObj = (e, type) => {
-        let rect =
-            this.imageRef.getBoundingClientRect();
+        let rect = this.imageRef.getBoundingClientRect();
         const xOffset = e.clientX - rect.left;
         const yOffset = e.clientY - rect.top;
         let stateObj = {};
@@ -120,7 +109,8 @@ class MemeMaker extends Component {
                 bottomX: `${xOffset}px`,
                 bottomY: `${yOffset}px`
             }
-        } else if (type === "top") {
+        }
+        else if (type === "top") {
             stateObj = {
                 isTopDragging: true,
                 isBottomDragging: false,
@@ -144,7 +134,8 @@ class MemeMaker extends Component {
             let stateObj = {};
             if (type === "bottom" && this.state.isBottomDragging) {
                 stateObj = this.getStateObj(e, type);
-            } else if (type === "top" && this.state.isTopDragging) {
+            }
+            else if (type === "top" && this.state.isTopDragging) {
                 stateObj = this.getStateObj(e, type);
             }
             this.setState({
@@ -153,14 +144,13 @@ class MemeMaker extends Component {
         }
     };
 
-    handleMouseUp = (event) => {
+    handleMouseUp = () => {
         document.removeEventListener('mousemove', this.handleMouseMove);
         this.setState({
             isTopDragging: false,
             isBottomDragging: false
         });
     }
-
 
     getBase64Image(img) {
         var canvas = document.createElement("canvas");
@@ -173,13 +163,8 @@ class MemeMaker extends Component {
     }
 
     render() {
-        // console.log(this.state.images);
-        //let images = JSON.stringify(this.props.userImages);
-        //const image = this.state.currentImage.image;
         const base_image = new Image();
-        // base_image.src = image.src;
-        base_image.crossOrigin="anonymous";
-        //var wrh = base_image.width / base_image.height;
+        base_image.crossOrigin = "anonymous";
         var newWidth = 600;
         var newHeight = 400;
         const textStyle = {
@@ -192,104 +177,70 @@ class MemeMaker extends Component {
         }
 
         return (
-            <div >
-
-
+            <div>
                 <div className="row col-12" id="mainBodyDiv">
-                <div className="col-12" id="navbarDiv">
-            <NavBar/>
-            </div>
-                    <div id="userProfileDiv">
-
-                        <UserProfile dMount={this.componentDidMount} sessionName={this.props.sessionName} sessionImage={this.props.sessionImage}/>
+                    <div className="col-12" id="navbarDiv">
+                        <NavBar />
                     </div>
-                    
+                    <div id="userProfileDiv">
+                        <UserProfile dMount={this.componentDidMount} sessionName={this.props.sessionName} sessionImage={this.props.sessionImage} />
+                    </div>
+
                     <div id="memeCardDiv">
                         {this.state.images &&
-                        <div className="content">
-                            {this.state.images.map((image, index) => (
-                                <div className="image-holder" key={image.id}>
-                                    <img crossOrigin="anonymous"
-                                        style={{
-                                            width: "100%",
-                                            cursor: "pointer",
-                                            height: "100%"
-                                        }}
-                                        alt={index}
-                                        src={image.image}
-                                        onClick={() => this.openImage(image)}
-                                        role="presentation"
-                                    />
-                                    <button className="btn btn-primary pinkButton" onClick={() => this.openImage(image)}>Make Meme</button>
-                                </div>
-                            ))}
+                            <div className="content">
+                                {this.state.images.map((image, index) => (
+                                    <div className="image-holder" key={image.id}>
+                                        <img crossOrigin="anonymous"
+                                            style={{
+                                                width: "100%",
+                                                cursor: "pointer",
+                                                height: "100%"
+                                            }}
+                                            alt={index}
+                                            src={image.image}
+                                            onClick={() => this.openImage(image)}
+                                            role="presentation"
+                                        />
+                                        <button className="btn btn-primary pinkButton" onClick={() => this.openImage(image)}>Make Meme</button>
+                                    </div>
+                                ))}
 
-                        </div>
+                            </div>
                         }
                     </div>
 
                     <Modal className="meme-gen-modal" isOpen={this.state.modalIsOpen}>
-          <ModalHeader toggle={this.toggle}>Make-a-Meme</ModalHeader>
-          <ModalBody>
-            <svg
-              width={newWidth}
-              id="svg_ref"
-              height={newHeight}
-              ref={el => { this.svgRef = el }}
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink">
-              <image
-              className="memeCard2Image"
-                ref={el => { this.imageRef = el }}
-                xlinkHref={this.state.currentImagebase64}
-                
-              />
-              <text
-                style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }}
-                x={this.state.topX}
-                y={this.state.topY}
-                dominantBaseline="middle"
-                textAnchor="middle"
-                onMouseDown={event => this.handleMouseDown(event, 'top')}
-                onMouseUp={event => this.handleMouseUp(event, 'top')}
-              >
-                  {this.state.toptext}
-              </text>
-              <text
-                style={textStyle}
-                dominantBaseline="middle"
-                textAnchor="middle"
-                x={this.state.bottomX}
-                y={this.state.bottomY}
-                onMouseDown={event => this.handleMouseDown(event, 'bottom')}
-                onMouseUp={event => this.handleMouseUp(event, 'bottom')}
-              >
-                  {this.state.bottomtext}
-              </text>
-            </svg>
-            <div className="meme-form">
-              <FormGroup>
-                <Label for="toptext">Top Text</Label>
-                <input className="form-control roundedInput" type="text" name="toptext" id="toptext" placeholder="Add text to the top" onChange={this.changeText} />
-              </FormGroup>
-              <FormGroup>
-                <Label for="bottomtext">Bottom Text</Label>
-                <input className="form-control roundedInput" type="text" name="bottomtext" id="bottomtext" placeholder="Add text to the bottom" onChange={this.changeText} />
-              </FormGroup>
-              {/* <button onClick={() => this.convertSvgToImage()} className="btn btn-primary">Download</button> */}
-              <div className="outerDivForPinkButton">
-              <button onClick={this.saveMeme} className="btn btn-primary pinkButton">Save</button>
-              </div>
-            </div>
-          </ModalBody>
-        </Modal>
+                        <ModalHeader toggle={this.toggle}>Make-a-Meme</ModalHeader>
+                        <ModalBody>
+                            <svg width={newWidth} id="svg_ref" height={newHeight} ref={el => { this.svgRef = el }} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                                <image className="memeCard2Image" ref={el => { this.imageRef = el }} xlinkHref={this.state.currentImagebase64} />
+                                <text style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }} x={this.state.topX} y={this.state.topY} dominantBaseline="middle" textAnchor="middle" onMouseDown={event => this.handleMouseDown(event, 'top')} onMouseUp={event => this.handleMouseUp(event, 'top')}>
+                                    {this.state.toptext}
+                                </text>
+                                <text style={textStyle} dominantBaseline="middle" textAnchor="middle" x={this.state.bottomX} y={this.state.bottomY} onMouseDown={event => this.handleMouseDown(event, 'bottom')} onMouseUp={event => this.handleMouseUp(event, 'bottom')}>
+                                    {this.state.bottomtext}
+                                </text>
+                            </svg>
+                            <div className="meme-form">
+                                <FormGroup>
+                                    <Label for="toptext">Top Text</Label>
+                                    <input className="form-control roundedInput" type="text" name="toptext" id="toptext" placeholder="Add text to the top" onChange={this.changeText} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="bottomtext">Bottom Text</Label>
+                                    <input className="form-control roundedInput" type="text" name="bottomtext" id="bottomtext" placeholder="Add text to the bottom" onChange={this.changeText} />
+                                </FormGroup>
+                                <div className="outerDivForPinkButton">
+                                    <button onClick={this.saveMeme} className="btn btn-primary pinkButton">Save</button>
+                                </div>
+                            </div>
+                        </ModalBody>
+                    </Modal>
                 </div>
             </div>
         )
     }
-
-
 }
-
 
 export default MemeMaker;
