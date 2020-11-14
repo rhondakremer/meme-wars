7701fa574753d60038b3cf4c196aa3c997ea5adf
KREMER <331943@dadeschools.net>
2020-11-14 16:35:21 -0500

    battle page container to functional component, hooks

--- client/src/containers/BattlePage/index.js
+++ client/src/containers/BattlePage/index.js
@@ -1,50 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import UserProfile from '../../components/UserProfile';
 import Api from '../../utils/API';
 import NavBar from '../../components/NavBar';
 import MemeCard2 from "../../components/MemeCard2";
 import "./style.css";
 
-class BattlePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUser: "",
-            myMemes: []
-        }
+const BattlePage = (props) => {
+    const [user, setUser] = useState(null);
+    const [memes, setMemes] = useState([]);
+
+    useEffect(() => {
+        let currentUser = JSON.parse(localStorage.getItem('session'));
+        setUser(currentUser.id);
+        getMemes();
+    }, [user]);
+
+    const getMemes = () => {
+        Api.getMemes(user)
+            .then(res => {
+                setMemes(res.data)
+            })
     }
 
-    componentDidMount() {
-        Api.getUsers()
-        .then(res => {
-            let user = JSON.parse(localStorage.getItem('session'));
-            this.setState({ currentUser: user.id }, () =>
-                Api.getMemes(this.state.currentUser)
-                .then(res => this.setState({ myMemes: res.data }))
-            )
-        });
-    }
-
-    render() {
-        return (<div>
+    return (
+        <div>
             <div className="col-12" id="navbarDiv">
                 <NavBar />
             </div>
             <div className="row col-12" id="mainBodyDiv">
                 <div id="userProfileDiv">
-                    <UserProfile componentDidMount={this.componentDidMount} sessionName={this.props.sessionName} sessionImage={this.props.sessionImage} />
+                    <UserProfile sessionName={props.sessionName} sessionImage={props.sessionImage} />
                 </div>
                 <div id="memeCardDivOnBattlePage">
-                    {this.state.myMemes.map((item) => (
+                    {memes.map((item) => (
                         <div>
-                            {console.log(item.bottomX)}
-                            <MemeCard2 id={item._id} src={item.baseImgURL} topX={item.topX} topY={item.topY} bottomY={item.bottomY} bottomX={item.bottomX} topText={item.topText} bottomText={item.bottomText} />
+                            <MemeCard2 id={item._id} key={item._id} src={item.baseImgURL} topX={item.topX} topY={item.topY} bottomY={item.bottomY} bottomX={item.bottomX} topText={item.topText} bottomText={item.bottomText} />
                         </div>
                     ))}
                 </div>
             </div>
-        </div>);
-    }
+        </div>
+    );
 }
 
 export default BattlePage;
