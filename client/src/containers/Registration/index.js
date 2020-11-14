d6c32bc0e56acce3d1228d6c5942fc8b2599f7e1
KREMER <331943@dadeschools.net>
2020-11-14 16:35:52 -0500

    invite friends, login, and reg containers to functional

--- client/src/containers/Registration/index.js
+++ client/src/containers/Registration/index.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import RegistrationForm from '../../components/RegistrationForm';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class Registration extends Component {
-    render() {
-        return <div className="loginContainer">
+const Registration = (props) => {
+    return (
+        <div className="loginContainer">
             <div className="logo"></div>
-            <RegistrationForm onRegister={this.props.onRegister} />
+            <RegistrationForm onRegister={props.onRegister} />
             <br />
             <div>
                 <h8>Already have an account?</h8>
                 <Link to="/login" className="nav-link registerAccount" id="registerLink">Login here.</Link>
             </div>
         </div>
-    }
+    )
 }
 
 export default Registration;
