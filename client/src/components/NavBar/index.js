c13ebe9d51934afdbaca765d6053bef1a74dd1e0
KREMER <331943@dadeschools.net>
2020-11-14 19:33:06 -0500

    navbar, pending wars, user picture, user profile to functional components w/ hooks

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,70 +1,64 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Api from '../../utils/API';
 import "./styles.css";
 
-class NavBar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentUser: "",
-      pending: ""
-    }
-  }
+const NavBar = () => {
+  const [user, setUser] = useState(null);
+  const [pendingBattles, setPendingBattles] = useState(null);
 
-  componentDidMount() {
-    Api.getUsers()
+  useEffect(() => {
+    let user = JSON.parse(localStorage.getItem('session'));
+    setUser(user.id);
+    getChallenges();
+  }, [user]);
+
+  const getChallenges = () => {
+    Api.getMyChallenges(user)
       .then(res => {
-        let user = JSON.parse(localStorage.getItem('session'));
-        this.setState({ currentUser: user.id }, () =>
-          Api.getMyChallenges(this.state.currentUser)
-            .then(res => this.setState({ pending: Object.keys(res.data).length })));
+        setPendingBattles(res.data.length);
       })
   }
 
-  logout = () => {
+  const logout = () => {
     localStorage.clear();
-    alert("logging out");
+    window.location.href='/login';
   }
 
-  logout = () => {
-    localStorage.clear();
-    document.location.reload();
-  }
+  return (
+    <div id="navBar">
+      <ul className="nav justify-content-start">
+        <li className="nav-item">
+          <Link to="/" className="nav-link active">Home</Link>
+        </li>
 
-  render() {
-    return (
-      <div id="navBar">
-        <ul className="nav justify-content-start">
-          <li className="nav-item">
-            <Link to="/" className="nav-link active">Home</Link>
-          </li>
+        <li className="nav-item">
+          <Link to="/mememaker" className="nav-link ">Initiate Battle!</Link>
+        </li>
 
-          <li className="nav-item">
-            <Link to="/mememaker" className="nav-link ">Initiate Battle!</Link>
-          </li>
+        <li className="nav-item">
+          <Link to="/pendingwars" className="nav-link active">Pending Wars</Link>
+        </li>
 
-          <li className="nav-item">
-            <Link to="/pendingwars" className="nav-link active">Pending Wars</Link>
-          </li>
-          {this.state.pending > 0 &&
-            <div id="pendingNumber">{this.state.pending}</div>
-          }
-          <li className="nav-item">
-            <Link to="/battle" className="nav-link active">My Vault</Link>
-          </li>
+        {pendingBattles > 0 &&
+          <div id="pendingNumber">{pendingBattles}</div>
+        }
+        
+        <li className="nav-item">
+          <Link to="/battle" className="nav-link active">My Vault</Link>
+        </li>
 
-          <li className="nav-item">
-            <Link to="/invite" className="nav-link active">Invite Friends</Link>
-          </li>
+        <li className="nav-item">
+          <Link to="/invite" className="nav-link active">Invite Friends</Link>
+        </li>
 
-          <li className="nav-item" id="logOutButton">
-            <button className="nav-link active" onClick={this.logout}>Log out</button>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+        <li className="nav-item" id="logOutButton">
+          <button className="nav-link active" onClick={logout}>Log out</button>
+        </li>
+      </ul>
+    </div>
+  );
+
 }
 
 export default NavBar;
--- client/src/components/pendingWars/index.js
+++ client/src/components/pendingWars/index.js
@@ -1,56 +1,48 @@
-import React, { Component } from 'react';
-import "./style.css"
+import React from 'react';
+import './style.css';
 
-class PendingWars extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUser: "",
-            imagebase64: ""
-        }
+const PendingWars = (props) => {
+    const textStyle = {
+        fontFamily: "Impact",
+        fontSize: "50px",
+        textTransform: "uppercase",
+        fill: "#FFF",
+        stroke: "#000",
+        userSelect: "none",
+        // check if isTopDragging does something here, seems like just left over from a copy paste situation
+        // zIndex: 1
     }
 
-    render() {
-        const textStyle = {
-            fontFamily: "Impact",
-            fontSize: "50px",
-            textTransform: "uppercase",
-            fill: "#FFF",
-            stroke: "#000",
-            userSelect: "none"
-        }
+    return (
+        <div>
+            <div id="svg_ref">
+                <div className="memeCard2Image" style={{ backgroundImage: "url(" + props.src + ")" }}>
+                    <svg>
+                        <text
+                            style={{ ...textStyle }}
+                            x={props.topX}
+                            y={props.topY}
+                            dominantBaseline="middle"
+                            textAnchor="middle"
 
-        return (
-            <div>
-                <div id="svg_ref">
-                    <div className="memeCard2Image" style={{ backgroundImage: "url(" + this.props.src + ")" }}>
-                        <svg >
-                            <text
-                                style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }}
-                                x={this.props.topX}
-                                y={this.props.topY}
-                                dominantBaseline="middle"
-                                textAnchor="middle"
-
-                            >
-                                {this.props.topText}
-                            </text>
-                            <text
-                                style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }}
-                                dominantBaseline="middle"
-                                textAnchor="middle"
-                                x={this.props.bottomX}
-                                y={this.props.bottomY}
-                            >
-                                {this.props.bottomText}
-                            </text>
-                        </svg>
-                    </div>
+                        >
+                            {props.topText}
+                        </text>
+                        <text
+                            style={{ ...textStyle }}
+                            dominantBaseline="middle"
+                            textAnchor="middle"
+                            x={props.bottomX}
+                            y={props.bottomY}
+                        >
+                            {props.bottomText}
+                        </text>
+                    </svg>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-export default PendingWars
+export default PendingWars;
 
